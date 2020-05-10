4181caa07c61807c0bc999951017589cc1d2657e
juan <juandambra@programming4design.com>
2020-05-10 02:04:26 -0300

    added integration to login endpoint

--- app/auth.py
+++ app/auth.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, request
+import requests
 
 bp = Blueprint('auth', __name__)
+login_endpoint = "https://chotuve-grupo2-auth-server-dev.herokuapp.com/login/"
 
 @bp.route('/login/', methods=["POST","GET"])
 def login():
     username = request.form['username']
     password = request.form['password']
-    if username == "admin" and password == "admin":
-        return "OK", 200
+    response = requests.post(login_endpoint, json={"username":username,"password":password})
+    if response.status_code == 200:
+        return response.json(), 200
     else:
         return "BAD LOGIN", 400
