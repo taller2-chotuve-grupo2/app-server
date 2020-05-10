cd79c25e1f03a330a51fea40f758c1c80174f764
juan <juandambra@programming4design.com>
2020-05-09 21:00:24 -0300

    added entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-echo "Waiting for postgres..."
+# echo "Waiting for postgres..."
 
-while ! nc -z db 5432; do
-  sleep 0.1
-done
+# while ! nc -z db 5432; do
+#   sleep 0.1
+# done
 
-echo "PostgreSQL started"
+# echo "PostgreSQL started"
 
-python app.py
+# python app.py
+flask run
