66e73812cb4eb2ff6a238148d8bc6c041fac2829
juan <juandambra@programming4design.com>
2020-05-10 01:30:38 -0300

    added passing test

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 bp = Blueprint('auth', __name__)
 
 @bp.route('/login/', methods=["POST","GET"])
 def login():
-    return "BAD LOGIN", 400
+    username = request.form['username']
+    password = request.form['password']
+    if username == "admin" and password == "admin":
+        return "OK", 200
+    else:
+        return "BAD LOGIN", 400
